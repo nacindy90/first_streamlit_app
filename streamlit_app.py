7f885b13571f5191066554c5d8c4dd9eb4fe361c
nacindy90 <141454069+nacindy90@users.noreply.github.com>
2023-08-04 13:27:16 -0700

    Update streamlit_app.py
    Added smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
